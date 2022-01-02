dd0eb5a8acbadbd0c8f30eed46b4e5824470560b
jojoldu <jojoldu@gmail.com>
2022-01-02 23:36:42 +0900

    App 기본 화면 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import RegionsContainer from './components/regions/RegionsContainer';
+import CategoriesContainer from './components/categories/CategoriesContainer';
+import RestaurantsContainer from './components/restaurants/RestaurantsContainer';
+
 export default function App() {
   return (
-    <></>
+    <div>
+      <h1>레스토랑 목록</h1>
+      <RegionsContainer />
+      <CategoriesContainer />
+      <RestaurantsContainer />
+    </div>
   );
 }
