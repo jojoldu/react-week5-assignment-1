860a92ee0a31a9066097658c3209a8c14928f9ee
jojoldu <jojoldu@gmail.com>
2022-01-02 23:13:24 +0900

    reducer fetchRegions 테스트 추가

--- src/reducer/tests/reducer.test.js
+++ src/reducer/tests/reducer.test.js
@@ -1,4 +1,5 @@
 import reducer from '../reducer';
+import { getFetchRegionsAction } from '../action/actions';
 
 describe('reducer', () => {
   it('입력된 state가 없으면 기본값이 반환된다', () => {
@@ -7,5 +8,17 @@ describe('reducer', () => {
     expect(state.regions).toStrictEqual([]);
     expect(state.categories).toStrictEqual([]);
     expect(state.restaurants).toStrictEqual([]);
+  });
+
+  describe('fetchRegions', () => {
+    it('regions이 조회된다', () => {
+      const expectName = '서울';
+      const state = reducer({}, getFetchRegionsAction([
+        { id: 1, name: expectName },
+      ]));
+
+      expect(state.regions).toHaveLength(1);
+      expect(state.regions[0].name).toBe(expectName);
+    });
   });
 });
