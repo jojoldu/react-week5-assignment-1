8dc3ccfa26666c787fcd1ea18142e6a876ae974f
jojoldu <jojoldu@gmail.com>
2022-01-02 23:11:50 +0900

    RegionsContainer useEffect 테스트 추가

--- src/components/regions/tests/RegionsContainer.test.jsx
+++ src/components/regions/tests/RegionsContainer.test.jsx
@@ -15,6 +15,7 @@ describe('RegionsContainer', () => {
 
   beforeEach(() => {
     useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({}));
   });
 
   afterEach(() => {
@@ -35,5 +36,11 @@ describe('RegionsContainer', () => {
     expect(result).toHaveLength(2);
     expect(result[0]).toHaveTextContent(expectName1);
     expect(result[1]).toHaveTextContent(expectName2);
+  });
+
+  it('render 하는 시점에 dispatch가 1회 수행된다', () => {
+    render(<RegionsContainer />);
+
+    expect(dispatch).toBeCalledTimes(1);
   });
 });
